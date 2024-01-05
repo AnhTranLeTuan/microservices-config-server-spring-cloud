f23ffcf7ffe6c1727ecc3011e787a8965392e9a6
Anh Tran Le Tuan <anh.letuan.tran.computer@gmail.com>
2024-01-05 07:53:18 -0600

    first commit

---
+++ src/main/java/project/microservices/configserverspringcloud/ConfigServerSpringCloudApplication.java
@@ -0,0 +1,15 @@
+package project.microservices.configserverspringcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+@EnableConfigServer
+@SpringBootApplication
+public class ConfigServerSpringCloudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConfigServerSpringCloudApplication.class, args);
+	}
+
+}
---
+++ src/test/java/project/microservices/configserverspringcloud/ConfigServerSpringCloudApplicationTests.java
@@ -0,0 +1,13 @@
+package project.microservices.configserverspringcloud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigServerSpringCloudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
